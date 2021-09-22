36ea893ec4a94f0239c3610b4b44ff9fd34c14b1
Nguyễn Quốc Trọn <90500313+QuocTron@users.noreply.github.com>
2021-09-22 21:07:40 +0700

    upload

---
+++ PROJECT/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/tron.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" 
+        integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+        <link rel="stylesheet" type ="text/css" href="./css/style.css">
+    <link rel="stylesheet" href="./css/responsive.css">
+    <title>SELFTAUGHT | Skill Website | CSS and SASS</title>
+</head>
+<body>
+    <header>
+        <div class ="header__logo-box">
+            <img src="img/logo.png" class ="header__logo" alt="logo">
+        </div>
+        <div class ="header__text-box">
+            <h3 class= "heading-primary">
+                <span class="heading-primary--main">selftaught</span>
+                <span class="heading-primary--sub">is how you survive</span>
+            </h3>
+            <a href="#" class ="btn btn--white btn--animated">See our courses</a>
+        </div>
+    </header>
---
+++ PROJECT/welcome.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to me</title>
+</head>
+<body>
+    <h1>CHúc mừng #</h1>
+</body>
+</html>
